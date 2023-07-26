a2f2c4e8c9aab0a13471faa1a03931dcc28f8957
Nathanael Gandhi <nat11public@gmail.com>
2023-07-26 16:07:56 +0000

    style: black formatting

--- app/app.py
+++ app/app.py
@@ -1,13 +1,16 @@
 # Python inheritance example
 class Vehicle:
-    def __init__(self, started = False, speed = 0):
+    def __init__(self, started=False, speed=0):
         self.started = started
         self.speed = speed
+
     def start(self):
         self.started = True
         print("Started, let's ride!")
+
     def stop(self):
         self.speed = 0
+
     def increase_speed(self, delta):
         if self.started:
             self.speed = self.speed + delta
@@ -15,26 +18,32 @@ class Vehicle:
         else:
             print("You need to start me first")
 
+
 # Creating a Python class
 # Modified into Creating your own Python constructor
 # Modified into Python inheritance example
 class Car(Vehicle):
     trunk_open = False
+
     def open_trunk(self):
         self.trunk_open = True
+
     def close_trunk(self):
         self.trunk_open = False
+
 
 # Overriding Python methods
 # Modified into Overriding other methods
 class Motorcycle(Vehicle):
-    def __init__(self, center_stand_out = False):
+    def __init__(self, center_stand_out=False):
         self.center_stand_out = center_stand_out
         super().__init__()
+
     def start(self):
         print("Sorry, out of fuel!")
 
-print('Hello world')
+
+print("Hello world")
 
 # Create a Python object
 car = Car()
