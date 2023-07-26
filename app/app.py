eb77fed536e3a7a4f7be0521eac9ce98551241b5
Nathanael Gandhi <nat11public@gmail.com>
2023-07-26 12:37:12 +0000

    feat: Creating your own Python constructor

--- app/app.py
+++ app/app.py
@@ -1,7 +1,9 @@
 # Creating a Python class
+# Modified into Creating your own Python constructor
 class Car:
-    speed = 0
-    started = False
+    def __init__(self, started = False, speed = 0):
+        self.started = started
+        self.speed = speed
     def start(self):
         self.started = True
         print("Car started, let's ride!")
@@ -32,3 +34,9 @@ car1.start()
 car1.increase_speed(10)
 print(car1.speed)
 print(car2.speed)
+
+# Creating your own Python constructor
+c1 = Car()
+c2 = Car(True)
+c3 = Car(True, 50)
+c4 = Car(started=True, speed=40)
